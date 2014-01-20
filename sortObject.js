fc31bd0668393b0b06529cb0fdc2a80af2321220
zndream17 <949572554@qq.com>
2014-01-20 17:38:58 +0800

    Sort Object Method

---
+++ sortObject.js
@@ -0,0 +1,21 @@
+var sortObject = function(o) {
+	
+	var sorted = {},
+		key, 
+		a = [];
+
+	for (key in o) {
+		if (o.hasOwnProperty(key)) {
+			a.push(key);
+		}
+	}
+
+	a.sort();
+
+	for (key = 0, len = a.lenght; key < len; key ++) {
+		sorted[a[key]] = o[a[key]];
+	}
+
+	return sorted;
+
+}
